7eb19d8f29d9aaba5142dae36b9d6ad623327982
Joshua Tate <jmtate@icloud.com>
2016-09-10 15:36:55 -0600

    finished ch19 silver challenge

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,11 +4,13 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var bootstrapPath = 'bower_components/bootstrap-sass/assets/';
+  var faPath = 'bower_components/font-awesome/scss/';
   var app = new EmberApp(defaults, {
     // Add options here
     sassOptions: {
       includePaths: [
-	bootstrapPath + 'stylesheets'
+	bootstrapPath + 'stylesheets',
+	faPath
       ]
     }
   });
