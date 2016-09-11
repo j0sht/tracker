79727d681a9d85daf6d901bbce50bb950d32779c
Joshua Tate <jmtate@icloud.com>
2016-09-10 19:08:43 -0600

    generated routes

---
+++ app/routes/index.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+// The extend method
+//  creates a new cubclass of an Ember.Route
+//  and accepts a JS object as its argument
+export default Ember.Route.extend({
+});
